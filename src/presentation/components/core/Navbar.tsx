99b73a71bd23a8d2c55c58fdd87fae64fb8e708a
dygopa <ylansaurio@gmail.com>
2023-02-27 12:10:16 -0400

    Change logo commit

--- src/presentation/components/core/Navbar.tsx
+++ src/presentation/components/core/Navbar.tsx
@@ -7,8 +7,8 @@ import { AuthRoutesEnum } from '@/lib/routes/authRoutes'
 const Navbar = () => {
   return (
     <div className='fixed top-0 left-0 w-full z-20 border-b bg-white max-h-[10vh] h-[10vh] p-[0.5%_10%] box-border flex justify-between items-center'>
-      <Link className='w-[10%] h-full object-contain' href="/">
-        <img src='/vercel.svg' className='w-full h-full object-contain'/>
+      <Link className='w-[15%] h-full object-contain' href="/">
+        <img src='https://i0.wp.com/medhaus.com.mx/wp-content/uploads/2022/09/cropped-IMG_5684.jpg?fit=927%2C927&ssl=1' className='w-full h-full object-cover object-center'/>
       </Link>
       <div className="lg:hidden sm:block w-[90%] h-full"></div>
       <div className="sm:hidden lg:flex justify-end items-center w-[90%] h-full gap-7 box-border">
